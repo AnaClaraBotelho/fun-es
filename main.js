b6d37c1b43ccf3f842c28de36131bf7ae8b772a0
AnaClaraBotelho Ana Clara <anaclaraltbdias@outlook.com>
2025-05-08 14:29:54 +0000

    commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// bloco de código que desempenha um papel especifico que só será executado quando for chamado
+/*
+EXEMPLO 1
+function mensagem {
+alert (“Seja muito bem-vindo(a)!”)
+console. Log(“Mais uma vez, seja bem-vindo(a)”)
+}
+mensagem ()
+*/
+
+/*
+EXEMPLO 2
+function somar (valor1, valor2) {
+alert(valor1 + valor2)
+}
+somar (30, 40)
+*/
+
+/*
+EXEMPLO 3 
+function mensagemPessoal (nome, cargo, anosTrabalhados) ‹ alert(‘Seja bem-vindo(a) ${cargo} $ {nome}. Estamos felizes por ter você trabalhando conosco há ${anosTrabalhados} anos. *)
+mensagemPessoal (“Lucas”, “Gerente”, 10) mensagemPessoal (“Irineu”, “Você não sabe, nem eu”, 50)
+*/
+
